e6313b0bbc8bb54b35349303e8e2fc2fe9cccfdc
alper_soy <soy.m.alper@gmail.com>
2019-12-24 22:01:36 +0300

    Adding Modals to our application

--- client-app/src/app/stores/rootStore.ts
+++ client-app/src/app/stores/rootStore.ts
@@ -3,6 +3,7 @@ import UserStore from "./userStore";
 import CommenStore from "./commonStore";
 import { createContext } from "react";
 import { configure } from "mobx";
+import ModalStore from "./modalStore";
 
 configure({ enforceActions: 'always' });
 
@@ -10,10 +11,12 @@ export class RootStore {
     activityStore: ActivityStore;
     userStore: UserStore;
     commonStore: CommenStore;
+    modalStore: ModalStore
     constructor() {
         this.activityStore = new ActivityStore(this);
         this.userStore = new UserStore(this);
         this.commonStore = new CommenStore(this);
+        this.modalStore = new ModalStore(this);
     }
 }
 
