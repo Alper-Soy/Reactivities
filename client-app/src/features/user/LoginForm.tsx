e531d5e68ec9f3c1e5157d36728e243f37846f59
alper_soy <soy.m.alper@gmail.com>
2019-12-24 20:09:14 +0300

    Dealing with submission errors

--- client-app/src/features/user/LoginForm.tsx
+++ client-app/src/features/user/LoginForm.tsx
@@ -1,19 +1,44 @@
 import React, { useContext } from "react";
 import { Form as FinalForm, Field } from "react-final-form";
-import { Form, Button } from "semantic-ui-react";
+import { Form, Button, Header } from "semantic-ui-react";
 import TextInput from "../../app/common/form/TextInput";
 import { RootStoreContext } from "../../app/stores/rootStore";
 import { IUserFormValues } from "../../app/models/user";
+import { FORM_ERROR } from "final-form";
+import { combineValidators, isRequired } from "revalidate";
+import ErrorMessage from "../../app/common/form/ErrorMessage";
+
+const validate = combineValidators({
+  email: isRequired("Email"),
+  password: isRequired("Password")
+});
 
 const LoginForm = () => {
   const rootStore = useContext(RootStoreContext);
   const { login } = rootStore.userStore;
-
   return (
     <FinalForm
-      onSubmit={(values: IUserFormValues) => login(values)}
-      render={({ handleSubmit }) => (
-        <Form onSubmit={handleSubmit}>
+      onSubmit={(values: IUserFormValues) =>
+        login(values).catch(error => ({
+          [FORM_ERROR]: error
+        }))
+      }
+      validate={validate}
+      render={({
+        handleSubmit,
+        submitting,
+        submitError,
+        invalid,
+        pristine,
+        dirtySinceLastSubmit
+      }) => (
+        <Form onSubmit={handleSubmit} error>
+          <Header
+            as='h2'
+            content='Login to Reactivities'
+            color='teal'
+            textAlign='center'
+          />
           <Field name='email' component={TextInput} placeholder='Email' />
           <Field
             name='password'
@@ -21,10 +46,23 @@ const LoginForm = () => {
             placeholder='Password'
             type='password'
           />
-          <Button positive content='Login' />
+          {submitError && !dirtySinceLastSubmit && (
+            <ErrorMessage
+              error={submitError}
+              text='Invalid email or password'
+            />
+          )}
+          <Button
+            disabled={(invalid && !dirtySinceLastSubmit) || pristine}
+            loading={submitting}
+            color='teal'
+            content='Login'
+            fluid
+          />
         </Form>
       )}
     />
   );
 };
+
 export default LoginForm;
